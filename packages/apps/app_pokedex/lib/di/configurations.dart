2746c62faf7e53f241e7fd7d4829c87d3f66a8e4
devsoupe <luvyum4e@gmail.com>
2025-03-20 17:20:37 +0900

    added get_it dependency module

--- packages/apps/app_pokedex/lib/di/configurations.dart
+++ packages/apps/app_pokedex/lib/di/configurations.dart
@@ -1,3 +1,8 @@
+import 'package:common/common.dart';
+import 'package:core/core.dart';
+import 'package:data/data.dart';
+import 'package:design/design.dart';
+import 'package:domain/domain.dart';
 import 'package:feature_pokedex/feature_pokedex.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
@@ -9,5 +14,10 @@ final getIt = GetIt.instance;
 @InjectableInit()
 void configureDependencies() {
   configureFeaturePokedexDependencies(getIt);
+  configureCommonDependencies(getIt);
+  configureCoreDependencies(getIt);
+  configureDomainDependencies(getIt);
+  configureDataDependencies(getIt);
+  configureDesignDependencies(getIt);
   getIt.init();
 }

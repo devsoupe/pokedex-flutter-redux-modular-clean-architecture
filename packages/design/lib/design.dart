d975710b5a251c436c1eb2273ead18697bcd4c7d
양초한 <chohan.yang@cjconnect.io>
2025-03-20 17:42:07 +0900

    Add style dictionary & foundation folder

--- packages/design/lib/design.dart
+++ packages/design/lib/design.dart
@@ -1,1 +1,3 @@
+export 'src/components/button.dart';
+export 'src/components/custom_card.dart';
 export 'src/di/configurations.dart';
--- packages/features/feature_pokedex_widgetbook/lib/src/usecases/button.dart
+++ packages/features/feature_pokedex_widgetbook/lib/src/usecases/button.dart
@@ -1,4 +1,4 @@
-import 'package:design/components/button.dart' show Button;
+import 'package:design/design.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;

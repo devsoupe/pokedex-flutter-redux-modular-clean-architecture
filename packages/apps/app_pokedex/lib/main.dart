415c7291b0ae01cd6c4852c2e67ac1ff62cfc693
devsoupe <luvyum4e@gmail.com>
2025-03-20 14:43:46 +0900

    added get_it, injectable

--- packages/apps/app_pokedex/lib/main.dart
+++ packages/apps/app_pokedex/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:feature_pokedex/feature_pokedex.dart';
 import 'package:flutter/material.dart';
 
+import 'di/configurations.dart';
+
 void main() {
+  configureDependencies();
   runApp(const FeaturePokedex());
 }

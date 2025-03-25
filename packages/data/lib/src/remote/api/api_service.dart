d112fab00060e6b2bbdcf298f07e306454d88250
devsoupe <luvyum4e@gmail.com>
2025-03-25 09:55:30 +0900

    add talker, dio-talker

--- packages/data/lib/src/remote/api/api_service.dart
+++ packages/data/lib/src/remote/api/api_service.dart
@@ -1,44 +1,39 @@
-// import 'package:data/src/remote/api/request_interceptor.dart';
-// import 'package:dio/dio.dart';
-// import 'package:flutter/foundation.dart';
-// // import 'package:pretty_dio_logger/pretty_dio_logger.dart';
-// import 'package:retrofit/retrofit.dart';
-//
-// @RestApi()
-// abstract class ApiService {
-//   factory ApiService() {
-//     final prettyDioLogger = PrettyDioLogger(
-//       request: true,
-//       requestHeader: true,
-//       requestBody: true,
-//       responseHeader: true,
-//       error: true,
-//       compact: true,
-//     );
-//
-//     final baseOptions = BaseOptions(
-//         connectTimeout: const Duration(seconds: 5),
-//         receiveTimeout: const Duration(seconds: 5));
-//
-//     Dio dio = Dio();
-//     dio.options = baseOptions;
-//     dio.interceptors.clear();
-//     dio.interceptors.add(RequestInterceptor());
-//     // dio.interceptors.add(TokenInterceptor(dio));
-//
-//     if (!kReleaseMode) {
-//       dio.interceptors.add(prettyDioLogger);
-//     }
-//
-//     return _ApiService(dio, baseUrl:"https://jsonplaceholder.typicode.com");
-//   }
-//
-//   // @GET("/posts")
-//   // Future<dynamic> getPost();
-//   //
-//   // @GET("/token")
-//   // Future<dynamic> getRefreshToken();
-//   //
-//   // @GET("/token/revoke")
-//   // Future<dynamic> signOut();
-// }
+import 'package:data/src/remote/api/request_interceptor.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:talker/talker.dart';
+import 'package:talker_dio_logger/talker_dio_logger.dart';
+
+@RestApi()
+abstract class ApiService {
+  // factory ApiService() {
+  //   final apiLogger = TalkerDioLogger(talker: Talker(), settings: const TalkerDioLoggerSettings());
+  //
+  //   final baseOptions = BaseOptions(connectTimeout: const Duration(seconds: 5), receiveTimeout: const Duration(seconds: 5));
+  //
+  //   Dio dio = Dio();
+  //   dio.options = baseOptions;
+  //   dio.interceptors.clear();
+  //   dio.interceptors.add(RequestInterceptor());
+  //   // dio.interceptors.add(TokenInterceptor(dio));
+  //
+  //   if (!kReleaseMode) {
+  //     dio.interceptors.add(apiLogger);
+  //   }
+  //
+  //   final baseUrl = dotenv.env['BASE_URL'];
+  //
+  //   return _ApiService(dio, baseUrl: baseUrl);
+  // }
+
+  // @GET("/posts")
+  // Future<dynamic> getPost();
+  //
+  // @GET("/token")
+  // Future<dynamic> getRefreshToken();
+  //
+  // @GET("/token/revoke")
+  // Future<dynamic> signOut();
+}
